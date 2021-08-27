58a614ec900dd21e121b69066aba364f3dad5437
José Carlos Martins <jcmartins81@outlook.com>
2021-08-27 15:45:35 -0300

    style: add repositories.scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
 import { RepositoryList } from "./components/RepositoryList";
-import { Counter } from "./components/Counter";
+
 import "./styles/global.scss";
 
 export function App() {
   return (
     <>
       <RepositoryList />
-      <Counter />
     </>
   );
 }
